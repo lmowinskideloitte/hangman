0b1d9662383d9394d8d575d155b676214342437b
Lukasz Mowinski <lmowinski@deloittece.com>
2022-07-13 11:00:48 +0200

    feat(GameLogic): hasWon added

--- src/main/java/org/example/GameLogic.java
+++ src/main/java/org/example/GameLogic.java
@@ -1,14 +1,24 @@
 package org.example;
 
-import java.util.Locale;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class GameLogic {
-    protected static boolean isGoodGuess(String word, char guessedChar) {
+    private static boolean isGoodGuess(String word, Character guessedChar) {
     String wordUpp = word.toUpperCase(Locale.ROOT);
     String guessedStringUpp = Character.toString(guessedChar).toUpperCase(Locale.ROOT);
 
-    return Character.isLetterOrDigit(guessedChar) ^ wordUpp.contains(guessedStringUpp);
+    return (Character.isLetterOrDigit(guessedChar) && wordUpp.contains(guessedStringUpp));
     }
 
+    protected static boolean hasWon(String word, ArrayList<Character> goodGuesses) {
+        String notLowerAlphaNum = "[^a-z\\d]";
+        String[] alphaNumWord = word.toLowerCase().replaceAll(notLowerAlphaNum, "").split("");
+
+        String uqSortedWord = Arrays.stream(alphaNumWord).distinct().sorted().collect(Collectors.joining());
+        String sortedGuesses = goodGuesses.stream().sorted().map(String::valueOf).collect(Collectors.joining());
+
+        return uqSortedWord.equals(sortedGuesses);
+    }
 
 }
